21f2cddb7165a21a8a1b443754155a911c83ad41
CharlieMGC <carlosmanuelgonzales211@gmial.com>
2024-08-26 11:21:11 -0400

    avanzando

--- lexer.cs
+++ lexer.cs
@@ -1,58 +1,182 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
+
+public enum TokenType
+{
+    // Literales
+    NUMBER,
+    STRING,
+    CHARACTER,
+
+    // Identificadores
+    ID,
+
+    // Operadores
+    PLUS,       // +
+    MINUS,      // -
+    TIMES,      // *
+    DIVIDE,     // /
+    MODULO,     // %
+    EQUAL,      // ==
+    NOT_EQUAL,  // !=
+    LESS_THAN,  // <
+    GREATER_THAN, // >
+    LESS_EQUAL,  // <=
+    GREATER_EQUAL, // >=
+    AND,        // &&
+    OR,         // ||
+    NOT,        // !
+    ASSIGN,     // =
+    PLUS_ASSIGN, // +=
+    MINUS_ASSIGN, // -=
+    TIMES_ASSIGN, // *=
+    DIVIDE_ASSIGN, // /=
+
+    // Separadores
+    COMMA,      // ,
+    SEMICOLON,  // ;
+    COLON,      // :
+
+    // Corchetes
+    LPAREN,     // (
+    RPAREN,     // )
+    LBRACE,     // {
+    RBRACE,     // }
+    LBRACKET,   // [
+    RBRACKET,   // ]
+
+    // Comentarios
+    COMMENT,    // // o /* ... */
+
+    // Palabras clave
+    KEYWORD,    // if, else, while, for, return, etc.
+}
+
+public class Token
+{
+    public TokenType Type { get; }
+    public string Value { get; }
+
+    public Token(TokenType type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type}: {Value}";
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        string rutaArchivo = @"D:\Compilador desde 0\newtest.txt";
+        string rutaArchivo = @"D:\Compilador desde 0\newtest.txt"; 
         string texto = File.ReadAllText(rutaArchivo);
         var tokens = Lexer(texto);
 
         Console.WriteLine("Tokens:");
         foreach (var token in tokens)
         {
-            Console.WriteLine($"Tipo: {token.Tipo}, Valor: {token.Valor}");
+            Console.WriteLine(token);
         }
     }
 
     static List<Token> Lexer(string texto)
     {
         List<Token> tokens = new List<Token>();
-        string tokenActual = string.Empty;
+        int i = 0;
 
-        foreach (char c in texto)
+        while (i < texto.Length)
         {
-            if (char.IsLetterOrDigit(c))
+            char c = texto[i];
+
+            if (char.IsWhiteSpace(c))
             {
-                tokenActual += c;
+                i++;
+                continue;
+            }
+            else if (char.IsDigit(c))
+            {
+                string number = "";
+                while (i < texto.Length && char.IsDigit(texto[i]))
+                {
+                    number += texto[i];
+                    i++;
+                }
+                tokens.Add(new Token(TokenType.NUMBER, number));
+            }
+            else if (char.IsLetter(c))
+            {
+                string id = "";
+                while (i < texto.Length && char.IsLetterOrDigit(texto[i]))
+                {
+                    id += texto[i];
+                    i++;
+                }
+                tokens.Add(new Token(TokenType.ID, id));
             }
             else
             {
-                if (!string.IsNullOrEmpty(tokenActual))
+                switch (c)
                 {
-                    tokens.Add(new Token { Tipo = "Palabra", Valor = tokenActual });
-                    tokenActual = string.Empty;
+                    case '+':
+                        tokens.Add(new Token(TokenType.PLUS, c.ToString()));
+                        break;
+                    case '-':
+                        tokens.Add(new Token(TokenType.MINUS, c.ToString()));
+                        break;
+                    case '*':
+                        tokens.Add(new Token(TokenType.TIMES, c.ToString()));
+                        break;
+                    case '/':
+                        tokens.Add(new Token(TokenType.DIVIDE, c.ToString()));
+                        break;
+                    case '%':
+                        tokens.Add(new Token(TokenType.MODULO, c.ToString()));
+                        break;
+                    case '=':
+                        tokens.Add(new Token(TokenType.ASSIGN, c.ToString()));
+                        break;
+                    case ',':
+                        tokens.Add(new Token(TokenType.COMMA, c.ToString()));
+                        break;
+                    case ';':
+                        tokens.Add(new Token(TokenType.SEMICOLON, c.ToString()));
+                        break;
+                    case ':':
+                        tokens.Add(new Token(TokenType.COLON, c.ToString()));
+                        break;
+                    case '(':
+                        tokens.Add(new Token(TokenType.LPAREN, c.ToString()));
+                        break;
+                    case ')':
+                        tokens.Add(new Token(TokenType.RPAREN, c.ToString()));
+                        break;
+                    case '{':
+                        tokens.Add(new Token(TokenType.LBRACE, c.ToString()));
+                        break;
+                    case '}':
+                        tokens.Add(new Token(TokenType.RBRACE, c.ToString()));
+                        break;
+                    case '[':
+                        tokens.Add(new Token(TokenType.LBRACKET, c.ToString()));
+                        break;
+                    case ']':
+                        tokens.Add(new Token(TokenType.RBRACKET, c.ToString()));
+                        break;
+                    default:
+                        tokens.Add(new Token(TokenType.ID, c.ToString()));
+                        break;
                 }
-                if (!char.IsWhiteSpace(c))
-                {
-                    tokens.Add(new Token { Tipo = "Signo", Valor = c.ToString() });
-                }
+                i++;
             }
-        }
-
-        if (!string.IsNullOrEmpty(tokenActual))
-        {
-            tokens.Add(new Token { Tipo = "Palabra", Valor = tokenActual });
         }
 
         return tokens;
     }
-}
-
-class Token
-{
-    public string? Tipo { get; set; }
-    public string? Valor { get; set; }
 }

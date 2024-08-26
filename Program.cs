80ff3b91c9503eb082a2006c455b48582c02f147
CharlieMGC <carlosmanuelgonzales211@gmial.com>
2024-08-25 22:27:19 -0400

    recien empezado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"D:\Compilador desde 0\newtest.txt";
+        
+        if (File.Exists(filePath))
+        {
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine("Contenido del archivo:");
+            Console.WriteLine(content);
+        }
+        else
+        {
+            Console.WriteLine("El archivo no existe.");
+        }
+    }
+
+}
---
+++ lexer.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TokenType
+{
+    
+
+
+}
